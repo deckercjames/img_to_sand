e9903b8fc66bf370b77e18dc0396bebd961a1adc
James Decker <deckej5@rpi.edu>
2024-10-24 23:55:01 -0400

    converted zhang suen integration to numpy

--- src/linker/zhang_suen.py
+++ src/linker/zhang_suen.py
@@ -1,6 +1,5 @@
 
-from copy import deepcopy
-from src.utils import check_grid_element_safe
+from src.utils import check_numpy_grid_element_safe
 from src.utils import get_grid_mask_subtraction
 from src.utils import get_mask_with_inward_bleed
 from src.utils import grid_mask_to_str
@@ -14,14 +13,14 @@ import numpy as np
 def _get_neighbours(image, r, c):
     '''Return 8-neighbours of point p1 of picture, in order'''
     return [
-        check_grid_element_safe(image, r-1, c,   default=False),
-        check_grid_element_safe(image, r-1, c+1, default=False),
-        check_grid_element_safe(image, r,   c+1, default=False),
-        check_grid_element_safe(image, r+1, c+1, default=False),
-        check_grid_element_safe(image, r+1, c,   default=False),
-        check_grid_element_safe(image, r+1, c-1, default=False),
-        check_grid_element_safe(image, r,   c-1, default=False),
-        check_grid_element_safe(image, r-1, c-1, default=False),
+        check_numpy_grid_element_safe(image, r-1, c,   default=False),
+        check_numpy_grid_element_safe(image, r-1, c+1, default=False),
+        check_numpy_grid_element_safe(image, r,   c+1, default=False),
+        check_numpy_grid_element_safe(image, r+1, c+1, default=False),
+        check_numpy_grid_element_safe(image, r+1, c,   default=False),
+        check_numpy_grid_element_safe(image, r+1, c-1, default=False),
+        check_numpy_grid_element_safe(image, r,   c-1, default=False),
+        check_numpy_grid_element_safe(image, r-1, c-1, default=False),
     ]
 
 def _get_transition_count(neighbours):
@@ -52,7 +51,7 @@ def zhang_suen_errosion_itteration(image):
 
 
 def zhang_suen_errosion(grid_mask):
-    ret_grid_mask = deepcopy(grid_mask)
+    ret_grid_mask = grid_mask.copy()
     while True:
         any_cells_changed = zhang_suen_errosion_itteration(ret_grid_mask)
         if not any_cells_changed:
@@ -71,14 +70,14 @@ def get_area_blobs(erroded_grid_mask):
 
     for r in range(len(erroded_grid_mask) - 1):
         for c in range(len(erroded_grid_mask[r]) - 1):
-            if (check_grid_element_safe(erroded_grid_mask, r, c) and
-                    check_grid_element_safe(erroded_grid_mask, r, c+1) and
-                    check_grid_element_safe(erroded_grid_mask, r+1, c) and
-                    check_grid_element_safe(erroded_grid_mask, r+1, c+1)):
-                remaining_blobs[r][c] = True
-                remaining_blobs[r][c+1] = True
-                remaining_blobs[r+1][c] = True
-                remaining_blobs[r+1][c+1] = True
+            if (check_numpy_grid_element_safe(erroded_grid_mask, r, c) and
+                    check_numpy_grid_element_safe(erroded_grid_mask, r, c+1) and
+                    check_numpy_grid_element_safe(erroded_grid_mask, r+1, c) and
+                    check_numpy_grid_element_safe(erroded_grid_mask, r+1, c+1)):
+                remaining_blobs[r, c] = True
+                remaining_blobs[r, c+1] = True
+                remaining_blobs[r+1, c] = True
+                remaining_blobs[r+1, c+1] = True
     
     return remaining_blobs
 
@@ -89,13 +88,12 @@ def get_flood_fill_area_blobs(original_grid_mask, micro_blob_grid_mask, itterati
     # print(grid_mask_to_str(micro_blob_grid_mask))
     
     for i in range(itterations):
-        intrim_blob_grid_mask = deepcopy(micro_blob_grid_mask)
-        for r in range(len(micro_blob_grid_mask)):
-            for c in range(len(micro_blob_grid_mask[r])):
-                if (not micro_blob_grid_mask[r][c] and
-                        original_grid_mask[r][c] and
-                        any(_get_neighbours(micro_blob_grid_mask, r, c))):
-                    intrim_blob_grid_mask[r][c] = True
+        intrim_blob_grid_mask = micro_blob_grid_mask.copy()
+        for r, c in np.ndindex(micro_blob_grid_mask.shape):
+            if (not micro_blob_grid_mask[r, c] and
+                    original_grid_mask[r, c] and
+                    any(_get_neighbours(micro_blob_grid_mask, r, c))):
+                intrim_blob_grid_mask[r, c] = True
         micro_blob_grid_mask = intrim_blob_grid_mask
         # print("Restoration iteration {} of {}".format(i+1, itterations))
         # print(grid_mask_to_str(micro_blob_grid_mask))
@@ -104,7 +102,7 @@ def get_flood_fill_area_blobs(original_grid_mask, micro_blob_grid_mask, itterati
 
 def get_split_lines_and_blobs(blob_grid_mask, itterations):
     
-    erroded_grid_mask = deepcopy(blob_grid_mask)
+    erroded_grid_mask = blob_grid_mask.copy()
     
     for i in range(itterations):
         zhang_suen_errosion_itteration(erroded_grid_mask)
--- testing/unit_tests/test_zhang_suen/test_zhang_suen_integration.py
+++ testing/unit_tests/test_zhang_suen/test_zhang_suen_integration.py
@@ -1,7 +1,7 @@
 
 from src.linker.zhang_suen import get_split_lines_and_blobs
 
-from testing.unit_tests.testing_helpers import helper_mask_string_to_bool_mask
+from testing.unit_tests.testing_helpers import helper_mask_string_to_numpy_bool_mask
 from testing.unit_tests.testing_helpers import helper_pretty_mask_compare
 
 
@@ -17,7 +17,7 @@ def test_zhang_suen_basic():
         "   ######                ########    ",
         "      #####                          ",
     ]
-    test_mask = helper_mask_string_to_bool_mask(test_mask_str)
+    test_mask = helper_mask_string_to_numpy_bool_mask(test_mask_str)
     
     result_blob, result_line = get_split_lines_and_blobs(test_mask, 2)
     
@@ -62,7 +62,7 @@ def test_zhang_suen_two_double_connected_blobs():
         "       ###############       #############  ",
         "         ########              ########     ",
     ]
-    test_mask = helper_mask_string_to_bool_mask(test_mask_str)
+    test_mask = helper_mask_string_to_numpy_bool_mask(test_mask_str)
     
     result_blob, result_line = get_split_lines_and_blobs(test_mask, 2)
     
